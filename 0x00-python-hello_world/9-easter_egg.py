36303192a14600a1c090b018d8994fd0fecf8b53
dnyamwamu <dnyamwamu@gmail.com>
2024-02-01 08:59:46 -0800

    9. Easter Egg

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("The Zen of Python, by Tim Peters")
+print("The Zen of Python, by Tim Peters\nBeautiful > ugly.\nExplicit > implicit.\nSimple > complex.\nComplex > complicated.\nFlat > nested.\nSparse > dense.\nReadability matters.\nSpecial cases aren't special.\nPracticality beats purity.\nErrors should never pass silently.\nRefuse ambiguity.\nOne obvious way.\nNow > never.\nNever > now.\nIf hard to explain, it's bad.\nIf easy to explain, it may be good.\nNamespaces are great -- let's use more!")
 
