dc4eacb25714be8813cb183095e6e44ecb965e98
dnyamwamu <dnyamwamu@gmail.com>
2024-02-01 06:41:20 -0800

    6. Play with strings

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+# Use f-string to concatenate str1 and str2
+welcome_message = f"Welcome to {str1} {str2}!"
+# Print the welcome message
+print(welcome_message)
