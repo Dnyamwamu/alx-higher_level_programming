7ab03ebd464c703e8e7c236a9266feebd8a2979a
dnyamwamu <dnyamwamu@gmail.com>
2024-02-04 08:25:54 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
+
 def uppercase(s):
     result = ""
     for char in s:
-        # Check if the character is a lowercase letter
         if 'a' <= char <= 'z':
             # Convert the lowercase letter to uppercase using ASCII values
             result += chr(ord(char) - 32)
         else:
             result += char
-            # Print the result followed by a new line
+            # Print the entire string in uppercase followed by a newline
             print(result)
-            # Test the function
-            uppercase("hello")
-            uppercase("world")
