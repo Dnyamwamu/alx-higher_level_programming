38b6a624381b56eba6f92a55b7acfafde2b71c0e
dnyamwamu <dnyamwamu@gmail.com>
2024-02-17 04:19:01 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-add = __import__("add_0").add
-print("{} + {} = {}".format(a, b, add(a, b)))
+
+from add_0 import add
+
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -2,50 +2,44 @@
 def add(a, b):
         """My addition function
 
-            Args:
-                    a: first integer
-                            b: second integer
+        Args:
+            a: first integer
+            b: second integer
 
-                                Returns:
-                                        The return value. a + b
-                                            """
-                                                return (a + b)
+        Returns:
+            The return value. a + b
+         """
+         return (a + b)
 
+def sub(a, b):
+    """My subtraction function
+    Args:
+        a: first integer
+        b: second integer
 
-                                            def sub(a, b):
-                                                    """My subtraction function
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
 
-                                                        Args:
-                                                                a: first integer
-                                                                        b: second integer
+def mul(a, b):
+    """My multiplication function
 
-                                                                            Returns:
-                                                                                    The return value. a - b
-                                                                                        """
-                                                                                            return (a - b)
+    Args:
+        a: first integer
+        b: second integer
 
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
 
-                                                                                        def mul(a, b):
-                                                                                                """My multiplication function
-
-                                                                                                    Args:
-                                                                                                            a: first integer
-                                                                                                                    b: second integer
-
-                                                                                                                        Returns:
-                                                                                                                                The return value. a * b
-                                                                                                                                    """
-                                                                                                                                        return (a * b)
-
-
-                                                                                                                                    def div(a, b):
-                                                                                                                                            """My division function
-
-                                                                                                                                                Args:
-                                                                                                                                                        a: first integer
-                                                                                                                                                                b: second integer
-
-                                                                                                                                                                    Returns:
-                                                                                                                                                                            The return value. a / b
-                                                                                                                                                                                """
-                                                                                                                                                                                    return int(a / b)
+def div(a, b):
+    """My division function
+                                                                                                                             Args:
+                                                                                                                                  a: first integer
+                                                                                                                                  b: second integer
+                                                                                                                             Returns:
+                                                                                                                             The return value. a / b
+                                                                                                                             """
+                                                                                                                             return int(a / b)
