faebe0f4328db3679eae31c1b4c761d4706a66b6
dnyamwamu <dnyamwamu@gmail.com>
2024-02-26 10:17:28 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
-
 def magic_calculation(a, b):
+    from magic_calculation import add, sub
     if a < b:
         c = a + b
         for i in range(4, 6):
