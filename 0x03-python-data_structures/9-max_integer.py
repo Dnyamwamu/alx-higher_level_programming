b7266c84d10c31401a4749119e3473f255e479b7
dnyamwamu <dnyamwamu@gmail.com>
2024-02-27 10:01:00 -0800

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    # Check if the list is empty
+    if not my_list:
+        return None
+    else:
+        max_value = my_list[0]  # Initialize max_value with the first element
+        for num in my_list:
+            if num > max_value:
+                max_value = num  # Update max_value if a larger number is found
+        return max_value
