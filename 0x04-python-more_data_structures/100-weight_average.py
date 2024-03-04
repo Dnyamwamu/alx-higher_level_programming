45a0862cffc144ef1e1b31d22c07a7eb90bc5789
dnyamwamu <dnyamwamu@gmail.com>
2024-03-04 13:26:43 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -7,8 +7,8 @@ def weight_average(my_list=[]):
     total_score = 0
     total_weight = 0
     
-    for score, weight in my_list:
-        total_score += score * weight
-        total_weight += weight
+    for tup in my_list:
+        total_score += tup[0] * tup[1]
+        total_weight += tup[1]
     
-    return total_score / total_weight
+    return (total_score / total_weight)
