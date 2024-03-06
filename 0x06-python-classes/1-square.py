7445b2a912663389dea95a093b6f6cc1b8cd4344
dnyamwamu <dnyamwamu@gmail.com>
2024-03-06 08:16:02 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Square: A class that defines a square."""
+
+class Square:
+    """Empty class representing a square."""
+
+    def __init__(self, size):
+        """Initialization method with a private size attribute."""
+        self.__size = size
