e0bb2a3bdaebbf17ccee27ea8e64c768eefb0c44
dnyamwamu <dnyamwamu@gmail.com>
2024-03-06 08:24:19 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Square: A class that defines a square."""
+
+class Square:
+    """Empty class representing a square."""
+
+    def __init__(self, size=0):
+        """Initialization method with a private size attribute."""
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
