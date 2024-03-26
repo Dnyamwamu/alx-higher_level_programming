10a212c82320246dbd754127248f4062cfa7367c
dnyamwamu <dnyamwamu@gmail.com>
2024-03-26 10:39:59 -0700

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an inherited list class MyList."""
 
+
 class MyList(list):
     """A subclass of list with additional method print_sorted to print the list in sorted order."""
 
