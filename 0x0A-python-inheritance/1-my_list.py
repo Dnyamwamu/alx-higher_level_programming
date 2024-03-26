3974fe8e71d04031aa3840efdc1a266fa1eadf31
dnyamwamu <dnyamwamu@gmail.com>
2024-03-26 10:38:26 -0700

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Defines an inherited list class MyList."""
 
 class MyList(list):
     """A subclass of list with additional method print_sorted to print the list in sorted order."""
@@ -6,11 +7,3 @@ class MyList(list):
     def print_sorted(self):
         """Prints the list in sorted (ascending) order."""
         print(sorted(self))
-
-# Example usage:
-if __name__ == "__main__":
-    # Create an instance of MyList
-    my_list = MyList([3, 1, 4, 1, 5, 9, 2, 6, 5])
-
-    # Print the list in sorted order
-    my_list.print_sorted()
