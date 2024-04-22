48df2d2e0a1aa3daf228f51612213d9bee7f5036
dnyamwamu <dnyamwamu@gmail.com>
2024-04-22 09:13:12 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
-"""Module that lists all states starting with 'N' from MySQL database"""
-
-import MySQLdb
+"""Module that lists all states from the hbtn_0e_0_usa database."""
 import sys
-
-def list_states_starting_with_N(username, password, database):
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    # Execute SQL query to fetch states starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all rows from the result set
-    rows = cursor.fetchall()
-
-    # Display results
-    for row in rows:
-        print(row)
-
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
+import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
+    # Get MySQL credentials from command-line arguments
+    # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    # Execute the SQL query to retrieve all states sorted by id
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
 
-    list_states_starting_with_N(username, password, database)
+0x0F-python-object_relational_mapping/2-my_filter_states.py
+
+#!/usr/bin/python3
+
+"""Module that lists all states from the hbtn_0e_0_usa database."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    # Get MySQL credentials and search name from command-line arguments
+    # and # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+
+    # Execute the SQL query to retrieve states with the specified name
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+
+    # Fetch all rows and print the states
+    [print(state) for state in c.fetchall()]
