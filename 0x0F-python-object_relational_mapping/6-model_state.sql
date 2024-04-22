6b8919d744b03d12a9abf7521a552336ebd05a9e
dnyamwamu <dnyamwamu@gmail.com>
2024-04-22 10:48:18 -0700

    state table

--- 0x0F-python-object_relational_mapping/6-model_state.sql
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -1,10 +1,5 @@
 -- Create database hbtn_0e_6_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 USE hbtn_0e_6_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB;
 
 SHOW CREATE TABLE states;
